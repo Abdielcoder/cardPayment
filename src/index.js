75844d690296cec9156852ff0d33a7c31ffd0053
Abdiel Carrasco <dev@abdiel.me>
2023-01-14 18:42:17 -0800

    CARDPAY02 - CONFIG ROUTERS AND LAYOUTS

--- src/index.js
+++ src/index.js
@@ -1,7 +1,29 @@
 const express = require('express');
+const expbhs = require('express-handlebars');
+const path = require('path');
 
 const app = express();
 
+
+//Settings engine layout
+app.set('views',path.join(__dirname,'views'))//set views folder
+app.engine('.hbs', expbhs.engine({
+    //Set layOuts
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs'
+
+}));
+app.set('view engine', '.hbs');
+//Routes
+app.use(require('./routes/index'));
+//Static
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Middleware for enpoints
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 
 app.listen(3000,() => {
     console.log('Server running on port 3000');
